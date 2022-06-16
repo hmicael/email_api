8f6d3094c46aa36eec67d2551c1b24be68824159
henintsoa <handriamahadimby@gmail.com>
2022-06-16 14:22:14 +0300

    install nelmio/{cors, api}

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,6 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
