7b175807cc4f6f1fbfb08142c31b77914049f9d8
henintsoa <handriamahadimby@gmail.com>
2022-06-16 14:23:47 +0300

    install maker

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
