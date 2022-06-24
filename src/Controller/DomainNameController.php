486f0fb9db2aa41bc1be7ace5adae3b10094cbf3
henintsoa <handriamahadimby@gmail.com>
2022-06-24 14:47:59 +0300

    CRUD domain_name

--- src/Controller/DomainNameController.php
+++ src/Controller/DomainNameController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\DomainName;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
